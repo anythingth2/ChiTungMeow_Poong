3a38566b6273186b07a051adfc856aafac38cd5f
0891679592 <nepheart_1@hotmail.com>
2017-03-28 21:59:27 +0700

    method input arrow key

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,23 +1,37 @@
 package sample;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.stage.Stage;
 
+
+
 public class Main extends Application {
-
     @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
+    public void start(Stage primaryStage) {
+        scene.setOnKeyPressed(ke -> {
+            KeyCode keyCode = ke.getCode();
+            if (keyCode.equals(KeyCode.UP)) {
+                System.out.println("UP");
+            }
+            if (keyCode.equals(KeyCode.DOWN)) {
+                System.out.println("down");
+            }
+            if (keyCode.equals(KeyCode.LEFT)) {
+                System.out.println("left");
+            }
+            if (keyCode.equals(KeyCode.RIGHT)) {
+                System.out.println("right");
+            }
+        });
+
     }
-
-
     public static void main(String[] args) {
         launch(args);
     }
+
 }
