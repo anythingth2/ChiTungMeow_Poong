59be5d4a2412d4a77fcffbf1e7cbd0f245653497
ChiChaChai <chatchaishaetan@gmail.com>
2017-03-29 16:01:16 +0700

    test eiei

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,34 +1,22 @@
 package sample;
 
 import javafx.application.Application;
-import javafx.event.EventHandler;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.Border;
-import javafx.scene.layout.BorderStroke;
 import javafx.scene.layout.Pane;
-import javafx.scene.paint.Paint;
+
 import javafx.stage.Stage;
 
-import java.util.Date;
 
 public class Main extends Application {
 
-    public void eiei(String ss, Date ssd) {
 
-
-    }
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        //Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+
 
         ImageView imgv = new ImageView("bin\\logo-kmitl.jpg");
-
 
         Pane root = new Pane();
 
@@ -40,11 +28,7 @@ public class Main extends Application {
         primaryStage.show();
 
 
-        while (true) {
 
-
-            root.setTranslateX(root.getTranslateX() + 1);
-        }
 
     }
 
