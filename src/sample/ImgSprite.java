969be11b21f979532dcf5f835f1bf6e5590a9c0b
ChiChaChai <chatchaishaetan@gmail.com>
2017-04-01 19:39:02 +0700

    d

--- src/sample/ImgSprite.java
+++ src/sample/ImgSprite.java
@@ -4,5 +4,5 @@ package sample;
  * Created by ChiChaChai on 30/3/2560.
  */
 public interface ImgSprite {
-    String[] cat={"CatUp_09.png","CatUp_10.png","CatUp_11.png","CatUp_12.png"};
+    String[] cat={"sprite\\Character\\Cat\\Up\\CatUp_01.png","CatUp_10.png","CatUp_11.png","CatUp_12.png"};
 }
